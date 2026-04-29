414e13a0a890cfc584090f3ef236b2b702f6408b
kashif Ali <kashifalisultan4455@gmail.com>
2026-04-29 16:21:55 +0500

    Add files via upload
    SMIT-Assignment01

---
+++ part01.py
@@ -0,0 +1,75 @@
+#01 Print Name 
+print("Name: Kashif Ali\nFather Name: Muhammad Ali\nDate of Birth: 01-01-2000")
+
+
+#02 small bio using veriable 
+name = "Kashif Ali"
+age = 20
+education = "BS Computer Science"
+university = "Shaheed Benazir Bhutto University"
+city = "Karachi"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I am studying", education)
+print("I study at", university)
+print("I live in", city)
+
+
+#03 Arithmetic, Comparison, and Logical Operators
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > b))
+
+c = a
+c += b
+c -= b
+c *= b
+c /= b
+
+list1 = [1, 2, 3, 4, 5]
+print(2 in list1)
+print(10 not in list1)
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+
+
+
+
+#04 Marks and Percentage Calculation
+english = 75
+islamiat = 80
+maths = 70
+
+total_marks = 300
+obtained_marks = english + islamiat + maths
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("English Marks:", english)
+print("Islamiat Marks:", islamiat)
+print("Maths Marks:", maths)
+print("Total Marks:", total_marks)
+print("Obtained Marks:", obtained_marks)
+print("Percentage:", percentage)
